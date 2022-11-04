db107a3df9c8c9399a0fcccf5036c654c7148d49
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2022-11-04 20:15:03 +0300

    Update massa.sh

--- massa.sh
+++ massa.sh
@@ -545,7 +545,6 @@ clean () {
 # MAIN
 txt=$(echo -e "$header" | sed 's/^/                                /')
 txt=$(echo -e "${RED}$txt${NC}")"\n"
-txt+='\n'$(line2)
 txt+='\n'"${YLW}NOTE:${NC} To enable IPv6 on DigitalOcean, refer the link below:"
 txt+='\n'"${BLU}\xF0\x9F\x94\x97 https://docs.digitalocean.com/products/networking/ipv6/how-to/enable/#on-existing-droplets${NC}"
 txt+='\n'$(line2)
